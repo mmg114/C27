5af88613bdb3be91a1303bf9515f8e1a139a321f
Mauricio Munoz Guzman <mauricio.m@moabits.com>
2024-07-25 20:56:17 -0500

    Initial commit

---
+++ src/main/java/Trabajos/Trabajo1.java
@@ -0,0 +1,8 @@
+package Trabajos;
+
+//Hacer una aplicacion, que solicite datos de los estudiantes(3), dentro de los datos deben estar las notas.
+// y requiero que se calcule el promedio de los estudiantes.
+
+//Deben usar Clases y Objetos, y Scaner para llenar los datos de los estudiantes.
+public class Trabajo1 {
+}
